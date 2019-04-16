a03c7e8e0ece58aa73f007dec5268214bc30cfe2
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-04-16 12:29:16 -0400

    concert this added:

--- liri.js
+++ liri.js
@@ -1,3 +1,51 @@
+// ------------------------------------------------------------- UNIVERSALS -----------------------------------------------------------------//
+
+var operandum = process.argv[2];
+
+// ------------------------------------------------------------- CONCERT THIS -----------------------------------------------------------------//
+
+// Name of the venue
+// Venue location
+// Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+// Establish Variables and Require
+var axios = require("axios");
+
+var artist = process.argv[3];
+
+function concert() {
+    // AJAX CalL
+    axios
+        .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            // If the axios was successful...
+            console.log(response.data[0].venue.name);
+            console.log(response.data[0].venue.city + ", " + response.data[0].venue.country);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
+
+
+
+
+// ------------------------------------------------------------- SPOTIFY --------------------------------------------------------------------//
+
 // ----------- PSUEDO CODE ------------ //
 // 1) Set up a capture for what the user types in
 // 2) User types something in and it writes that to another file
@@ -8,7 +56,6 @@
 // *) Preview link of the song from Spotify
 // *) Album
 // 5) Once we have the API call working maybe setup a prompt to ask the user what they want to do for each field
-
 
 
 // Code to read and set any environment variables with the dotenv package:
@@ -42,8 +89,17 @@ spotify.search({ type: 'track', query: 'All the Small Things', limit: 1 }, funct
     console.log(JSON.stringify(data.tracks.items[0].album.name, null, 2));
 
 
-
 });
+
+
+// ------------------------------------------------------------- SWITCHBOARD -----------------------------------------------------------------//
+
+
+switch (operandum) {
+    case "concert":
+        concert();
+        break;
+}
 
 
 
